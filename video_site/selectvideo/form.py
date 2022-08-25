8737a5b6e13e05f87263871b9e5a55d1ce0e121a
Nathan Daniels <nathan18daniels@gmail.com>
2022-08-25 14:44:06 +0000

    Completed video selection

--- video_site/selectvideo/form.py
+++ video_site/selectvideo/form.py
@@ -1,20 +1,36 @@
 from django import forms
+from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 
-class UserForm(forms.Form):
+
+class UserForm(forms.ModelForm):
     email = forms.EmailField(label='Enter Email:', required=True)
-    password = forms.CharField(label='Enter Password:', widget=forms.PasswordInput, required=True, strip=True)
-    confirm = forms.CharField(label='Confirm Passowrd:', widget=forms.PasswordInput, required=True, strip=True)
-    
+    password = forms.CharField(
+        label='Enter Password:', widget=forms.PasswordInput, required=True, strip=True)
+    confirm = forms.CharField(
+        label='Confirm Password:', widget=forms.PasswordInput, required=True, strip=True)
+
+    def save(self, commit=True):
+        usr = super(UserForm, self).save(commit=False)
+
+        if commit:
+            usr = User.objects.create_user(usr.email, usr.email, usr.password)
+
+        return usr
+
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get("password")
         confirm = cleaned_data.get("confirm")
 
         if password and confirm:
-            
+
             if password != confirm:
                 raise forms.ValidationError(
                     _("Passwords don't match"),
                     code='invalid',
                 )
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'confirm')
--- video_site/video_site/urls.py
+++ video_site/video_site/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playback/', include('playback.urls')),
-    path('selectvideo/', include('selectvideo.urls'))
+    path('', include('selectvideo.urls'))
 ]
